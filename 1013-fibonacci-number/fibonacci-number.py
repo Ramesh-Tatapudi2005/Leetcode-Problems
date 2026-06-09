8c68ef7a94264c0fc02846b0d28f82cd822d8a3d
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-06-09 20:44:01 +0530

    Time: 46 ms (66.70%) | Memory: 19.1 MB (57.35%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,8 @@
 class Solution:
     def fib(self, n: int) -> int:
         f0, f1 = 0,1
-        if n == 0:
-            return 0
-        if n == 1:
-            return 1
-        for i in range(1,n):
-            f2= f1 + f0
-            f0,f1 = f1,f2
+        if n <= 1:
+            return n
+        for _ in range(1,n):
+            f0,f1 = f1,f1+f0
         return f1
