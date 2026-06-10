dfd4ff5364c94cd8dcc664751fd79d949d85389f
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-06-10 22:03:56 +0530

    Time: 67 ms (86.88%) | Memory: 33.9 MB (80.53%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.py
+++ 2245-destroying-asteroids/destroying-asteroids.py
@@ -1,9 +1,12 @@
 class Solution:
     def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
         asteroids.sort()
+        m = max(asteroids)
         for i in asteroids:
             if mass < i:
                 return False
             else:
                 mass += i
+                if mass >= m:
+                    return True
         return True
