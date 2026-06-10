a5d0b05c18a10e77058d0b6e955722cc4d4c559c
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-06-10 22:33:03 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (16.07%) - LeetSync

--- 507-perfect-number/perfect-number.py
+++ 507-perfect-number/perfect-number.py
@@ -1,10 +1,11 @@
 import math
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        s = 1
-        if num == 1 or num == 0:
-            return False
-        for i in range(2,int(math.sqrt(num))+1):
-            if num % i == 0:
-                s = s + i + (num//i)
-        return (s==num)
+        # s = 1
+        # if num == 1 or num == 0:
+        #     return False
+        # for i in range(2,int(math.sqrt(num))+1):
+        #     if num % i == 0:
+        #         s = s + i + (num//i)
+        # return (s==num)
+        return num in [6,28,496,8128,33550336,8589869056]
