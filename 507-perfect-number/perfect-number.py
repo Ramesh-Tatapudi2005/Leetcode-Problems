955595b02edddc9c38c041e5adb130dd7630934c
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-06-10 22:28:06 +0530

    Time: 4 ms (59.10%) | Memory: 19.2 MB (51.92%) - LeetSync

---
+++ 507-perfect-number/perfect-number.py
@@ -0,0 +1,10 @@
+import math
+class Solution:
+    def checkPerfectNumber(self, num: int) -> bool:
+        s = 1
+        if num == 1 or num == 0:
+            return False
+        for i in range(2,int(math.sqrt(num))+1):
+            if num % i == 0:
+                s = s + i + (num//i)
+        return (s==num)
