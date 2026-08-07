ec2613ea99cecad856e05555519b2f50e5a4bfec
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-08-07 22:33:23 +0530

    Time: 62 ms (75.69%) | Memory: 19.7 MB (73.55%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,22 +2,22 @@ from collections import Counter
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         need = Counter(t)
-        window = {}
+        window ={}
         req = len(need)
-        form = 0
         l = 0
-        ans = float('inf'), 0 , 0
+        form = 0
+        ans = float('inf') , 0 , 0
         for r in range(len(s)):
             c = s[r]
-            window[c]  = window.get(c,0)+ 1
+            window[c] = window.get(c,0) + 1
             if c in need and window[c] == need[c]:
-                form += 1
-            while l <= r and req == form:
-                if r - l + 1 < ans[0]:
-                    ans = r- l+ 1 , l , r
+                form +=1
+            while l <= r and form == req:
+                if r - l  + 1 < ans[0]:
+                    ans =  r - l + 1 , l , r
                 c = s[l]
                 window[c] -= 1
                 if c in need and window[c] < need[c]:
-                    form -= 1
-                l += 1
-        return "" if ans[0] == float('inf') else s[ans[1]: ans[2]+1]
+                    form -=1 
+                l+=1
+        return "" if ans[0] == float('inf') else s[ans[1] : ans[2]+1]
