8301f297126684e6341650b75231eb19b86ce026
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-08-12 20:41:38 +0530

    Time: 207 ms (81.74%) | Memory: 23.9 MB (66.70%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,23 +1,22 @@
 class Solution:
-    def daysneeded(self, arr, cap):
-        totaldays = 1
-        load = 0
+    def calcdays(self, arr, cap):
+        days = 1
+        load = 0 
         for w in arr:
-            if w + load > cap:
-                totaldays += 1
+            if load + w > cap:
+                days+= 1
                 load = w
             else:
                 load += w
-        return totaldays
+        return days
     def shipWithinDays(self, weights: List[int], days: int) -> int:
         low = max(weights)
         high = sum(weights)
-        ans = -1
         while low < high:
             mid = (low + high) // 2
-            noofdays = self.daysneeded(weights, mid)
-            if noofdays <= days:
-                high = mid
+            totaldays = self.calcdays(weights,mid)
+            if totaldays <= days:
+                high = mid 
             else:
                 low = mid + 1
         return low
