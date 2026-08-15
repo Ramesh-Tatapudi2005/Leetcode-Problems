ac7ba4d54320927e06f574a0c46e36e82d1eef03
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-08-15 15:12:00 +0530

    Time: 71 ms (17.89%) | Memory: 22.2 MB (50.07%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -3,13 +3,24 @@ class Solution:
         zeros = 0
         left = right = 0
         ans = float('-inf')
+        # while right < len(nums):
+        #     if nums[right] == 0:
+        #         zeros += 1
+        #     while zeros > k:
+        #         if nums[left] == 0:
+        #             zeros -= 1
+        #         left += 1
+        #     ans = max(ans, right- left + 1)
+        #     right += 1
+        # return ans
         while right < len(nums):
             if nums[right] == 0:
                 zeros += 1
-            while zeros > k:
+            if zeros > k:
                 if nums[left] == 0:
                     zeros -= 1
                 left += 1
-            ans = max(ans, right- left + 1)
+
+            ans = max(ans, right - left + 1)
             right += 1
         return ans
