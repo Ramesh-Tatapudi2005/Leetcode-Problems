17da06bd29b1f9b43558184b17527fb29a2aeb34
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-09-02 15:43:00 +0530

    Time: 1 ms (31.03%) | Memory: 19.3 MB (63.08%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.py
+++ 2050-count-good-numbers/count-good-numbers.py
@@ -1,11 +1,19 @@
 import math
 class Solution:
+    def power(self,base, exp):
+        if exp == 0:
+            return 1
+        half = self.power(base,exp // 2)
+        if exp % 2 == 0:
+            return (half * half) % (10**9 + 7)
+        else:
+            return (half * half * base) % (10**9 + 7)
+        
     def countGoodNumbers(self, n: int) -> int:
         if n ==1:
             return 5
-        odd = even = math.ceil(n / 2)
-        if n % 2 == 1:
-            odd -= 1
         MOD = 10 ** 9 + 7
-        ans = (pow(5, even, MOD) * pow(4,odd, MOD))% MOD 
+        even = (n+1) // 2
+        odd = n // 2
+        ans = self.power(5,even) * self.power(4,odd) % MOD
         return ans 
