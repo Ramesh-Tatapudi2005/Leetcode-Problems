b6f4b997f2331af9258d87b09debb0cd6368e3cf
Ramesh-Tatapudi2005 <tatapudirameshramesh3@gmail.com>
2026-09-21 10:00:22 +0530

    Time: 43 ms (60.02%) | Memory: 34.4 MB (19.10%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -2,22 +2,23 @@ class Solution:
     def partition(self, s: str) -> list[list[str]]:
         result = []
         n = len(s)
-        def generateall(curr_part, ind):
+        def generatepart(ind, curr_part):
             if ind == n:
                 result.append(curr_part.copy())
-                return
+                return 
+            
             for i in range(ind, n):
                 if ispalindrome(s, ind, i):
-                    curr_part.append(s[ind: i + 1])
-                    generateall(curr_part, i+1)
+                    curr_part.append(s[ind: i +1])
+                    generatepart(i+1, curr_part)
                     curr_part.pop()
-
-        def ispalindrome(st, start, end):
+            
+        def ispalindrome(s, start,end):
             while start <= end:
-                if st[start] != st[end]:
+                if s[start] != s[end]:
                     return False
                 start += 1
                 end -= 1
             return True
-        generateall([],0)
+        generatepart(0, [])
         return result 
